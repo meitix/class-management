db80facf1ae6fe5053a5969fea7549f84f04a199
Meitix <m.valizadeh@live.com>
2019-06-05 17:09:13 +0800

    Code is formatted.

--- src/public/src/app/modules/school-management/models/edu/lesson.interface.ts
+++ src/public/src/app/modules/school-management/models/edu/lesson.interface.ts
@@ -1,8 +1,8 @@
-import { IEntity } from "../system/entity.interface";
-import { IPractice } from "./practice.interface";
+import { IEntity } from '../system/entity.interface';
+import { IPractice } from './practice.interface';
 
 export interface ILesson extends IEntity {
-    title: string;
-    practices: IPractice[];
-    description?: string;
+  title: string;
+  practices: IPractice[];
+  description?: string;
 }
