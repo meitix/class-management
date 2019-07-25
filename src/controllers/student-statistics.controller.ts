71bc6c7065e0fe78f26c87d7ee77d0627274d5f5
Meitix <m.valizadeh@live.com>
2019-07-25 10:22:06 +0800

    Get by student id and get by class status id has been added.

---
+++ src/controllers/student-statistics.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import {
+  ClassStatus,
+  StudentStatusSchema
+} from '../models/entities/student-status.entity';
+
+export class StudentStatisticsController {
+  async batchUpdate(req: Request, res: Response) {
+    const classStatusId = req.params.clStId;
+    try {
+      const result = await ClassStatus.findByIdAndUpdate(classStatusId, {
+        $set: { statistics: req.body }
+      });
+      res.end(result);
+    } catch (e) {
+      console.log(e);
+      res.status(400).send(e);
+    }
+  }
+
+  // get by student id.
+  async getByStudentId(req: Request, res: Response) {
+    try {
+      const result = await ClassStatus.find(
+        { 'statistics.student': req.params.studentId },
+        { 'statistic.$.result': 1 }
+      );
+      res.json(result);
+    } catch (e) {
+      console.log(e);
+      res.status(400).json(e);
+    }
+  }
+
+  // get by class status id.
+  async getByClassStatusId(req: Request, res: Response) {
+    try {
+      const result = await ClassStatus.findById(req.params.clStId);
+      res.json(result);
+    } catch (e) {
+      console.log(e);
+      res.status(400).json(e);
+    }
+  }
+}
