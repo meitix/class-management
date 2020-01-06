ed8e18900999c99a0dc410acd5a083ce7b93c801
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-06 14:50:54 +0330

    add authToken after user log in

--- src/public/src/app/modules/authentication/components/login/login.component.ts
+++ src/public/src/app/modules/authentication/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { catchError, tap } from 'rxjs/operators';
+import { SchoolService } from 'src/app/modules/school-management/components/school/services/school.service';
 
 @Component({
   selector: 'app-login',
@@ -19,40 +20,44 @@ export class LoginComponent {
 
   errorMessage: string;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private schoolService: SchoolService
+  ) {}
 
   submit() {
     if (this.form.valid) {
       this.isLoading = true;
-      this.authService
-        .login(this.username, this.password)
-        .subscribe(
-          res => {
-            // check user is exists.
-            if (res) {
-              // make user remember.
-              if (this.remember) {
-                this.authService.makeUserRemembered(res);
-              }
+      this.authService.login(this.username, this.password).subscribe(
+        res => {
+          // check user is exists.
+          if (res) {
+            // make user remember.
+            if (this.remember) {
+              this.authService.makeUserRemembered(res);
+            }
 
-              // make user logged in.
-              this.authService.setCurrentUser(res);
+            // make user logged in.
+            this.authService.setCurrentUser(res);
+            this.schoolService.appenAuthToken();
 
-              if (res.schoolId) {
-                // create redirect url based on user role.
-                this.router.navigate(['school', res.schoolId]);
-              } else {
+            if (res.schoolId) {
+              // create redirect url based on user role.
+              this.router.navigate(['school', res.schoolId]);
+            } else {
               // navigate to dashboard.
               this.router.navigate(['']);
-              }
-            } else {
-              this.errorMessage = 'نام کاربری یا کلمه عبور اشتباه است';
             }
-          },
-          e => {
-            this.errorMessage = e.error;
-          }, () => this.isLoading = false
-        );
+          } else {
+            this.errorMessage = 'نام کاربری یا کلمه عبور اشتباه است';
+          }
+        },
+        e => {
+          this.errorMessage = e.error;
+          this.isLoading = false;
+        }
+      );
     } else {
       this.errorMessage = 'لطفا موارد خواسته شده را کامل کنید';
     }
