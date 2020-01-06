217a7a474e1197d9f2085e029fd379cd33efb353
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-06 14:44:04 +0330

    change secretKey address

--- src/helpers/token.manager.ts
+++ src/helpers/token.manager.ts
@@ -1,20 +1,22 @@
-import  { resolve } from 'path';
-import {sign} from "jsonwebtoken";
-import { readFileSync } from "fs";
-import expressJWT = require("express-jwt");
+import { resolve } from 'path';
+import { sign } from 'jsonwebtoken';
+import { readFileSync } from 'fs';
+import expressJWT = require('express-jwt');
 
 class TokenManager {
   private secretKey: string;
 
   constructor() {
     // read secret key from config.
-    this.secretKey = readFileSync(resolve("src/config/settings/jwt-secret.key")).toString();
+    this.secretKey = readFileSync(
+      resolve('./config/settings/jwt-secret.key')
+    ).toString();
   }
 
   // generate the token.
   generate(data: any) {
     return sign(data, this.secretKey, {
-      expiresIn: (Math.floor(Date.now() / 1000) * 60 * 60)
+      expiresIn: Math.floor(Date.now() / 1000) * 60 * 60
     });
   }
 
