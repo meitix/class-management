3b038cfacc0c742150c40aa0c8173da9b82544bc
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-06 15:08:42 +0330

    change navigating for editing

--- src/public/src/app/modules/school-management/components/person/person-list/person-list.component.ts
+++ src/public/src/app/modules/school-management/components/person/person-list/person-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { IPerson } from '../../../models/people/person.interface';
 import { EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-person-list',
@@ -11,13 +13,22 @@ export class PersonListComponent implements OnInit {
   @Input() people: Array<IPerson>;
   // tslint:disable-next-line: no-output-rename
   @Output('delete') deleteEvent: EventEmitter<any>;
-  constructor() {
+  schoolIdSubscription: Subscription;
+  schoolId: string;
+  constructor(private router: Router, private route: ActivatedRoute) {
     this.deleteEvent = new EventEmitter();
   }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    this.schoolIdSubscription = this.route.params.subscribe(params => {
+      this.schoolId = params.id;
+    });
   }
-
+  navigateToEdit(id)
+  {
+    this.router.navigate([`/school/${this.schoolId}/student/edit/${id}`]);
+  }
   delete(person: any) {
     this.deleteEvent.emit(person);
   }
