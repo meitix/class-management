6b1e7c0a58ac07e751266534950f2af69081fe71
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-13 12:21:31 +0330

    add routs for get by code

--- src/routes/school.routes.ts
+++ src/routes/school.routes.ts
@@ -5,50 +5,75 @@ import classRoutes from './class.routes';
 import periodRoutes from './period.routes';
 
 class SchoolRouter {
-    router: Router;
-    private schoolController: SchoolController;
-    private peopleController: PeopleController;
+  router: Router;
+  private schoolController: SchoolController;
+  private peopleController: PeopleController;
 
-    constructor() {
-        this.router = Router({mergeParams: true});
-        this.schoolController = new SchoolController();
-        this.peopleController = new PeopleController();
-        this.init();
-    }
+  constructor() {
+    this.router = Router({ mergeParams: true });
+    this.schoolController = new SchoolController();
+    this.peopleController = new PeopleController();
+    this.init();
+  }
 
-    init() {
-        // Schools functionality is here.
-        // fetch.
-        this.router.get('/' , this.schoolController.fetch);
-        this.router.get('/:id' , this.schoolController.fetch);
-        // create.
-        this.router.post('/' , this.schoolController.create);
-        // edit.
-        this.router.put('/:id' , this.schoolController.update);
-        // delete.
-        this.router.delete('/:id' , this.schoolController.delete);
+  init() {
+    // Schools functionality is here.
+    // fetch.
+    this.router.get('/', this.schoolController.fetch);
+    this.router.get('/:id', this.schoolController.fetch);
+    // create.
+    this.router.post('/', this.schoolController.create);
+    // edit.
+    this.router.put('/:id', this.schoolController.update);
+    // delete.
+    this.router.delete('/:id', this.schoolController.delete);
 
-        // Students.
-        this.router.post('/:id/students' , this.peopleController.addStudent);
-        this.router.get('/student/:studentId' , this.peopleController.getStudent)
-        this.router.get('/:id/students' , this.peopleController.getStudents);
-        this.router.put('/:id/student/:studentId' , this.peopleController.updateStudent);
-        this.router.delete('/:id/student/:studentId' , this.peopleController.deleteStudent);
-        
+    // Students.
+    this.router.post('/:id/students', this.peopleController.addStudent);
+    this.router.get('/student/:studentId', this.peopleController.getStudent);
+    this.router.get(
+      '/:studentCode/student',
+      this.peopleController.getStudentByCode
+    );
+    this.router.get('/:id/students', this.peopleController.getStudents);
+    this.router.put(
+      '/:id/student/:studentId',
+      this.peopleController.updateStudent
+    );
+    this.router.delete(
+      '/:id/student/:studentId',
+      this.peopleController.deleteStudent
+    );
 
-        // Personnel.
-        this.router.post('/:id/personnel' , this.peopleController.addPersonnel);
-        this.router.get('/:id/personnel/:personnelId' , this.peopleController.getPersonnelById)
-        this.router.get('/:id/personnel' , this.peopleController.getPersonnel);
-        this.router.put('/:id/personnel/:personnelId' , this.peopleController.updatePersonnel);
-        this.router.delete('/:id/personnel/:personnelId' , this.peopleController.deletePersonnel);
-        this.router.get('/:id/personnel/search/:term' , this.peopleController.search);
-        
-        // class routes.
-        this.router.use('/:id/classes', classRoutes);
-        // period routes.
-        this.router.use('/:id/periods', periodRoutes);
-    }
+    // Personnel.
+    this.router.post('/:id/personnel', this.peopleController.addPersonnel);
+    this.router.get(
+      '/:id/personnel/:personnelId',
+      this.peopleController.getPersonnelById
+    );
+    this.router.get(
+      '/:id/:personnelCode/personnel',
+      this.peopleController.getPersonnelByCode
+    );
+    this.router.get('/:id/personnel', this.peopleController.getPersonnel);
+    this.router.put(
+      '/:id/personnel/:personnelId',
+      this.peopleController.updatePersonnel
+    );
+    this.router.delete(
+      '/:id/personnel/:personnelId',
+      this.peopleController.deletePersonnel
+    );
+    this.router.get(
+      '/:id/personnel/search/:term',
+      this.peopleController.search
+    );
+
+    // class routes.
+    this.router.use('/:id/classes', classRoutes);
+    // period routes.
+    this.router.use('/:id/periods', periodRoutes);
+  }
 }
 
 export default new SchoolRouter().router;
