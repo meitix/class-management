262faaf652f094a0e649444a4cf1b67db1264c1a
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-16 14:45:43 +0330

    change dbconectionString

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { ClassManagement } from "./class-management";
+import { ClassManagement } from './class-management';
 
 const classManagement = new ClassManagement();
 
-// classManagement.start(3000 , 'mongodb://localhost:27017/class-management'); // local cs;
-classManagement.start(3000 , 'mongodb+srv://nodeapp:@123qwe@@cluster0-mselx.mongodb.net/test?retryWrites=true&w=majority');
+classManagement.start(3000, 'mongodb://localhost:27017/class-management'); // local cs;
+// classManagement.start(3000 , 'mongodb+srv://nodeapp:@123qwe@@cluster0-mselx.mongodb.net/test?retryWrites=true&w=majority');
