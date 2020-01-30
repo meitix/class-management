cb205a8e0c266053de6d838906a52b9d93e4afc9
soheil najafi <soheilnajafimail2@gmail.com>
2020-01-30 18:32:23 +0330

    do managing in users dashboard when they loged in

--- src/public/src/app/modules/school-management/components/school/school-dashboard/school-dashboard.component.ts
+++ src/public/src/app/modules/school-management/components/school/school-dashboard/school-dashboard.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AuthService,
+  ILoginResult
+} from 'src/app/modules/authentication/services/auth.service';
+import { flatten } from 'lodash';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-school-dashboard',
@@ -6,10 +13,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./school-dashboard.component.css']
 })
 export class SchoolDashboardComponent implements OnInit {
+  currentUser: ILoginResult;
+  currentRoles = [];
+  schoolIdSubscription: Subscription;
+  schoolId: string;
+  manageClass: boolean;
+  studentsStatistics: boolean;
+  managePersonnel: boolean;
+  manageSchoolUP: boolean;
+  manageClassUP: boolean;
+  studentsStatisticsUP: boolean;
+  managePersonnelUP: boolean;
+  manageStudentsUP: boolean;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.schoolIdSubscription = this.route.params.subscribe(params => {
+      this.schoolId = params.id;
+    });
+    this.currentUser = this.authService.getCurrentUser();
+    this.currentRoles = flatten(
+      this.currentUser.roles.map(r => r.accessibility)
+    );
+
+    this.currentRoles.find(r => {
+      if (r.title === 'manage-school') {
+        return (this.manageSchoolUP = true);
+      } else {
+        if (r.title === 'manage-class') {
+          this.manageClass = true;
+        } else if (r.title === 'manage-class-statistics') {
+          this.studentsStatistics = true;
+        }
+        if (r.title === 'manage-personnel') {
+          this.managePersonnel = true;
+        }
+      }
+    });
   }
 
+  turnOnStudentsStatistics() {
+    this.studentsStatisticsUP = true;
+    this.manageSchoolUP = false;
+    this.manageClassUP = false;
+    this.managePersonnelUP = false;
+    this.manageStudentsUP = false;
+  }
+  turnOnManagePersonnel() {
+    this.studentsStatisticsUP = false;
+    this.manageSchoolUP = false;
+    this.manageClassUP = false;
+    this.managePersonnelUP = true;
+    this.manageStudentsUP = false;
+  }
+  turnOnManageClasses() {
+    this.studentsStatisticsUP = false;
+    this.manageSchoolUP = false;
+    this.manageClassUP = true;
+    this.manageStudentsUP = false;
+    this.managePersonnelUP = false;
+  }
+  turnOnManageStudents() {
+    this.studentsStatisticsUP = false;
+    this.manageSchoolUP = false;
+    this.manageClassUP = false;
+    this.manageStudentsUP = true;
+    this.managePersonnelUP = false;
+  }
+  // turnOnManageSchool() {
+  //   this.studentsStatisticsUP = false;
+  //   this.manageSchoolUP = true;
+  //   this.manageClassUP = false;
+  //   this.manageStudentsUP = false;
+  //   this.managePersonnelUP = false;
+  // }
 }
